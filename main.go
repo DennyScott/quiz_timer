2e4dcfe579dae39dc4e4fd1ff5121434d5085315
Denny Scott <dennyscott301@gmail.com>
2017-11-24 10:55:47 -0600

    Add a timer to the quiz

--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
 	flag.Parse()
 
 	file, err := os.Open(*csvFilename)
@@ -25,18 +27,33 @@ func main() {
 
 	problems := parseLines(lines)
 
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
 	correct := 0
 	for i, p := range problems {
 		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
+		answerCh := make(chan string)
 
-		if answer == p.a {
-			correct++
+		go scanAnswer(answerCh)
+
+		select {
+		case <-timer.C:
+			fmt.Printf("\nYou scored %d out of %d.\n", correct, len(problems))
+			return
+		case answer := <-answerCh:
+			if answer == p.a {
+				correct++
+			}
 		}
 	}
 
 	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+}
+
+func scanAnswer(answerCh chan string) {
+	var answer string
+	fmt.Scanf("%s\n", &answer)
+	answerCh <- answer
 }
 
 func parseLines(lines [][]string) []problem {
